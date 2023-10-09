47b4aba73885615ac0033cdada4bbb52b3c60685
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-10-10 06:10:23 +0800

    feat: Add first declaration of the 'selected' on top

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const menuItems = [...document.querySelectorAll('.menu-item')];
+let currentlySelected = document.querySelector('.selected');
 
 // Add feature that toggles which menu item is currently selected.
 menuItems.forEach(menuItem => {
     menuItem.addEventListener('click', () => {
         // Remove 'selected' class from the currently selected menu-item.
-        const currentlySelected = document.querySelector('.selected');
+        currentlySelected = document.querySelector('.selected');
         currentlySelected.classList.remove('selected');
 
         // Add the 'selected' class to the clicked menu-item.
