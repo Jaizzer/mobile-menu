53555617b0d631c247eece2f14e69633c517c80a
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-10-10 06:16:00 +0800

    feat: Add feature that shows the currently selected page

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const menuItems = [...document.querySelectorAll('.menu-item')];
 let currentlySelected = document.querySelector('.selected');
 
+// Add the name of the currently selected page to the 'current-page-indicator'.
+const currentPageIndicator = document.querySelector('.current-page-indicator');
+currentPageIndicator.textContent = currentlySelected.querySelector(".text").textContent;
+
 // Add feature that toggles which menu item is currently selected.
 menuItems.forEach(menuItem => {
     menuItem.addEventListener('click', () => {
@@ -10,5 +14,8 @@ menuItems.forEach(menuItem => {
 
         // Add the 'selected' class to the clicked menu-item.
         menuItem.classList.add('selected');
+
+        // Add the name of the currently selected page to the 'current-page-indicator'.
+        currentPageIndicator.textContent = menuItem.querySelector(".text").textContent;
     });
 });
